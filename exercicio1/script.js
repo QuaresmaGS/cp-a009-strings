0f776204b3cf3f92695dd1ded84432553aad100d
Gabriel <gabrielssquaresma@gmail.com>
2022-07-26 11:59:22 -0300

    exercicio 1, 2 e 3. Uso de propriedade e metodos de string

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nomeDoUsuario = prompt("Qual o seu nome?");
+const comidaFavorita1 = prompt("Qual a sua comida favorita?");
+const comidaFavorita2 = prompt("Qual outra?");
+const comidaFavorita3 = prompt("Mais uma?");
+
+console.log(`Estas são as comidas favoritas de ${nomeDoUsuario}: \n-${comidaFavorita1} \n-${comidaFavorita2} \n-${comidaFavorita3}`);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,6 @@
-//Crie a const para a frase aqui
+const mensagem1 = (`Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair" `);
+const nmensage1 = mensagem1.replace("verde", "rosa");
+const nNmensagem1 = nmensage1.replace("azul", "laranja");
+const gato = ("mas não deixe o gato sair");
+const nNMensagem1 = (`Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, ${gato.toUpperCase()}" `);
+console.log(nNMensagem1);
